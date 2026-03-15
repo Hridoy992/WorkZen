3f12eb2dfaf65c197bfed042035f53a85f656af0
Hridoy Saha <hridoy.cse.20220204059@aust.edu>
2026-03-15 15:06:02 +0600

    1 file changed

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -15,7 +15,7 @@ export const router = createBrowserRouter([
 ],
     {
         future: {
-            v7_relativeSplatPath: false,
+            v7_relativeSplatPath: true,
             v7_fetcherPersist: true,
             v7_normalizeFormMethod: true,
             v7_partialHydration: true,
